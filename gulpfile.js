4a9ee7dc3de6914e8dd956c3fa208b38f0f1e81a
Yamila Moreno <yamila.ms@gmail.com>
2021-08-03 17:27:13 +0200

    (feat) Serve Taiga in subpath

--- gulpfile.js
+++ gulpfile.js
@@ -6,47 +6,71 @@
  * Copyright (c) 2021-present Kaleidos Ventures SL
  */
 
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
+var gulp = require('gulp'),
+    coffee = require("gulp-coffee"),
+    concat = require("gulp-concat"),
+    uglify = require("gulp-uglify"),
+    plumber = require("gulp-plumber"),
+    sass = require("gulp-sass"),
+    cached = require("gulp-cached"),
+    remember = require('gulp-remember');
 
 var paths = {
-    styles: 'styles/*.scss',
-    coffee: 'coffee/*.coffee',
-    images: 'images/**/*',
-    dist: 'dist/'
+  styles: 'styles/*.scss',
+  coffee: 'coffee/*.coffee',
+  images: 'images/**/*',
+  dist: 'dist/'
 };
 
 gulp.task('copy-config', function() {
-    return gulp.src('call-to-action.json')
-        .pipe(gulp.dest(paths.dist));
+  return gulp
+    .src('call-to-action.json')
+    .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('copy-images', function() {
-    return gulp.src(paths.images)
-        .pipe(gulp.dest(paths.dist + "images"));
+  return gulp
+    .src(paths.images)
+    .pipe(gulp.dest(paths.dist + "images"));
 });
 
 gulp.task('compile-coffee', function() {
-    return gulp.src(paths.coffee)
-        .pipe($.plumber())
-        .pipe($.cached('coffee'))
-        .pipe($.coffee())
-        .pipe($.remember('coffee'))
-        .pipe($.concat('call-to-action.js'))
-        .pipe($.uglify({mangle:false, preserveComments: false}))
-        .pipe(gulp.dest(paths.dist));
+  return gulp
+    .src(paths.coffee)
+    .pipe(plumber())
+    .pipe(cached('coffee'))
+    .pipe(coffee())
+    .pipe(remember('coffee'))
+    .pipe(concat('call-to-action.js'))
+    .pipe(uglify({mangle:false, annotations: false}))
+    .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('compile-styles', function() {
-    return gulp.src(paths.styles)
-        .pipe($.sass({outputStyle: 'compressed'}).on('error', $.sass.logError))
-        .pipe(gulp.dest(paths.dist));
+  return gulp
+    .src(paths.styles)
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('watch', function() {
-    gulp.watch([paths.styles, paths.images, paths.coffee], ['compile-coffee', 'compile-styles']);
+gulp.task('watch', function(cb) {
+  gulp.watch(paths.styles, gulp.parallel(['compile-styles']));
+  gulp.watch(paths.coffee,gulp.parallel(['compile-coffee']));
+  gulp.watch(paths.images,gulp.parallel(['copy-images']));
+  cb();
 });
 
-gulp.task('default', ['copy-config', 'copy-images', 'compile-coffee', 'compile-styles', 'watch']);
+gulp.task('default', gulp.series(
+  'copy-config',
+  'copy-images',
+  'compile-coffee',
+  'compile-styles',
+  'watch'
+));
 
-gulp.task('build', ['copy-config', 'copy-images', 'compile-coffee', 'compile-styles']);
+gulp.task('build', gulp.series(
+  'copy-config',
+  'copy-images',
+  'compile-coffee',
+  'compile-styles'
+));
